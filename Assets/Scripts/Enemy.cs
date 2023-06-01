14d9123d06a0b352202551d5abbf7d407377368d
Alfred_Layug <alfredangelo.layug@benilde.edu.ph>
2023-06-02 00:46:08 +0800

    Only one sprite is used for the arrows, only its color and rotation changes. I also modified the code so that the arrow is yellow while spinning, and then it turns green or red after it stops spinning. Swipe detection now has a minimum range. I also updated the swipe detection code so that it can better detect swipes in all four directions. The enemy now dies as soon as the player makes a correct swipe. If the player swipes in the wrong direction, then they can no longer swipe again to kill the enemy.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-
     public int _health;
     public int _attack;
     public int _defense;
@@ -13,18 +12,181 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float startingXPosition;
     [SerializeField] private float startingYPosition;
 
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite arrowSprite;
+
+    [SerializeField] private bool inRange;
+
+    private Vector2 initialTouchPosition;
+    private Vector2 endTouchPosition;
+
     float timer;
     float movementSpeed;
+
+    Quaternion originalRotation;
+
+    public enum Direction { Left, Down, Right, Up }
+    public enum Color { Green, Red }
+
+    Direction arrowDirection;
+    Color arrowColor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inRange = true;
+        //Debug.Log(other.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inRange = false;    // Enemy can no longer be killed if it gets too close to the player
+    }
 
     void Start()
     {
         movementSpeed = 4.0f;
+
+        transform.position = new Vector2 (startingXPosition, startingYPosition);
+
+        inRange = false;
+        arrowDirection = (Direction)Random.Range(0, 4);
+        arrowColor = (Color)Random.Range(0, 2);
+        StartCoroutine(CO_Timer());
+
+        originalRotation = image.transform.rotation;
+
+        Debug.Log(arrowDirection);
+        Debug.Log(arrowColor);
     }
 
     private void Update()
     {
         startingYPosition = startingYPosition - (Time.deltaTime * movementSpeed);
 
-        transform.position = new Vector2 (startingXPosition, startingYPosition);
+        transform.Translate(Vector3.down * Time.deltaTime * movementSpeed, Space.World);
+
+        if (inRange == true)
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Touch touch = Input.GetTouch(0);
+                initialTouchPosition = touch.position;
+            }
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Touch touch = Input.GetTouch(0);
+                endTouchPosition = touch.position;
+
+                CheckSwipe();
+            }
+        }
+    }
+
+    private IEnumerator CO_Timer()
+    {
+        while (!inRange)
+        {
+            yield return new WaitForSeconds(0.2f);
+            image.sprite = arrowSprite;
+            image.GetComponent<Image>().color = new Color32(255, 255, 0, 100);  //Arrow will be color yellow while still rotating
+            image.transform.Rotate(0, 0, 90);
+        }
+
+        image.transform.rotation = originalRotation;
+        image.sprite = arrowSprite;
+
+        if (arrowColor == Color.Green)
+        {
+            image.GetComponent<Image>().color = new Color32(0, 255, 0, 100);
+
+            if (arrowDirection == Direction.Down)
+            {
+                image.transform.Rotate(0, 0, 90);
+            }
+            else if (arrowDirection == Direction.Right)
+            {
+                image.transform.Rotate(0, 0, 180);
+            }
+            else if (arrowDirection == Direction.Up)
+            {
+                image.transform.Rotate(0, 0, -90);
+            }
+            else
+            {
+                image.transform.Rotate(0, 0, 0);
+            }
+        }
+        else    // Rotations are opposite here since red arrows tell the player to swipe in the opposite direction
+        {
+            image.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+
+            if (arrowDirection == Direction.Down)
+            {
+                image.transform.Rotate(0, 0, -90);
+            }
+            else if (arrowDirection == Direction.Left)
+            {
+                image.transform.Rotate(0, 0, 180);
+            }
+            else if (arrowDirection == Direction.Up)
+            {
+                image.transform.Rotate(0, 0, 90);
+            }
+            else
+            {
+                image.transform.Rotate(0, 0, 0);
+            }
+        }
+    }
+
+    private void CheckSwipe()
+    {
+        Debug.Log("X " + Mathf.Abs(endTouchPosition.x - initialTouchPosition.x));
+        Debug.Log("Y " + Mathf.Abs(endTouchPosition.y - initialTouchPosition.y));
+
+        if (Mathf.Abs(endTouchPosition.y - initialTouchPosition.y) >
+            Mathf.Abs(endTouchPosition.x - initialTouchPosition.x) &&
+            Mathf.Abs(endTouchPosition.y - initialTouchPosition.y) >= 480)
+        {
+            if (initialTouchPosition.y < endTouchPosition.y && arrowDirection == Direction.Up)
+            {
+                Debug.Log("Swiped Up");
+                Destroy(gameObject);
+            }
+            else if (initialTouchPosition.y > endTouchPosition.y && arrowDirection == Direction.Down)
+            {
+                Debug.Log("Swiped Down");
+                Destroy(gameObject);
+            }
+            else
+            {
+                inRange = false;    //Player can no longer swipe after swiping in the wrong direction
+                Debug.Log("Wrong swipe, you got damaged.");
+            }
+        }
+        else if (Mathf.Abs(endTouchPosition.x - initialTouchPosition.x) >= 480)
+        {
+            if (initialTouchPosition.x < endTouchPosition.x && arrowDirection == Direction.Right)
+            {
+                Debug.Log("Swiped Right");
+                Destroy(gameObject);
+
+            }
+            else if (initialTouchPosition.x > endTouchPosition.x && arrowDirection == Direction.Left)
+            {
+                Debug.Log("Swiped Left");
+                Destroy(gameObject);
+            }
+            else
+            {
+                inRange = false;    //Player can no longer swipe after swiping in the wrong direction
+                Debug.Log("Wrong swipe, you got damaged.");
+            }
+        }
+        else
+        {
+            Debug.Log("Swipe not counted");
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,144 +5,23 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Image image;
-    [SerializeField] private Sprite[] sprites;
-
-    [SerializeField] private bool inRange;
-
-    float timer;
-
-    private Vector2 initialTouchPosition;
-    private Vector2 endTouchPosition;
-
-    private bool swipedLeft;
-    private bool swipedRight;
-    private bool swipedUp;
-    private bool swipedDown;
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
-
-        inRange = true;
+        //Debug.Log(other.name);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if ((image.sprite == sprites[1] || image.sprite == sprites[6]) && swipedLeft == true)    //Player must swipe left
-        {
-            Destroy(other.gameObject);
-            swipedLeft = false;
-        }
-
-        if ((image.sprite == sprites[2] || image.sprite == sprites[5]) && swipedRight == true)    //Player must swipe right
-        {
-            Destroy(other.gameObject);
-            swipedRight = false;
-        }
-
-        if ((image.sprite == sprites[0] || image.sprite == sprites[7]) && swipedDown == true)    //Player must swipe down
-        {
-            Destroy(other.gameObject);
-            swipedDown = false;
-        }
-
-        if ((image.sprite == sprites[3] || image.sprite == sprites[4]) && swipedUp == true)    //Player must swipe up
-        {
-            Destroy(other.gameObject);
-            swipedUp = false;
-        }
-
-        inRange = false;
-        StartCoroutine(CO_Timer());
-
         //SpawnManager.Instance.RemoveEnemyFromList(other.gameObject);
     }
 
     private void Start()
     {
         transform.position = new Vector2(0.0f, 0.0f);
-
-        inRange = false;
-        StartCoroutine(CO_Timer());
-
-        swipedLeft = false;
-        swipedRight = false;
-        swipedUp = false;
-        swipedDown = false;
     }
 
     private void Update()
     {
-
-        if (inRange == true)    //This is to prevent the player from swiping ahead of time
-        {
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                Touch touch = Input.GetTouch(0);
-                initialTouchPosition = touch.position;
-            }
-
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
-            {
-                Touch touch = Input.GetTouch(0);
-                endTouchPosition = touch.position;
-
-                CheckSwipe();
-            }
-        }
-    }
-
-    private IEnumerator CO_Timer()
-    {
-        while (!inRange)
-        {
-            yield return new WaitForSeconds(0.2f);
-            image.sprite = sprites[Random.Range(0, sprites.Length)];
-        }
-
-        image.sprite = sprites[Random.Range(0, sprites.Length)];
-    }
-
-    private void CheckSwipe()
-    {
-        if (image.sprite == sprites[0] || image.sprite == sprites[7] || image.sprite == sprites[3] || image.sprite == sprites[4])
-        {
-            if (initialTouchPosition.y < endTouchPosition.y)
-            {
-                Debug.Log("Swiped Up");
-                swipedRight = false;
-                swipedLeft = false;
-                swipedDown = false;
-                swipedUp = true;
-            }
-            else
-            {
-                Debug.Log("Swiped Down");
-                swipedRight = false;
-                swipedLeft = false;
-                swipedDown = true;
-                swipedUp = false;
-            }
-        }
-        else
-        {
-            if (initialTouchPosition.x < endTouchPosition.x)
-            {
-                Debug.Log("Swiped Right");
-                swipedRight = true;
-                swipedLeft = false;
-                swipedDown = false;
-                swipedUp = false;
-            }
-            else
-            {
-                Debug.Log("Swiped Left");
-                swipedRight = false;
-                swipedLeft = true;
-                swipedDown = false;
-                swipedUp = false;
-            }
-        }
+        
     }
 }
